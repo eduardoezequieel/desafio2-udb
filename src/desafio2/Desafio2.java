cf3a0ff251c92aff3e6ef94ad9a94da20fb36cfe
Eduardo Ezequiel <riv.edu10@gmail.com>
2023-10-23 00:18:26 -0600

    feat: first commit

---
+++ src/desafio2/Desafio2.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package desafio2;
+
+import desafio2.views.FrmLayout;
+
+/**
+ *
+ * @author Eduardo
+ */
+public class Desafio2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        FrmLayout layout = new FrmLayout();
+        layout.setVisible(true);
+    }
+    
+}
